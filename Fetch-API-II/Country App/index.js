34645ac7e2def6adad5dd7e371be9182a97b5cc9
Avinash Pawar <avipawar940524@gmail.com>
2024-07-01 10:20:52 +0530

    Updated country app

--- Fetch-API-II/Country App/index.js
+++ Fetch-API-II/Country App/index.js
@@ -1,34 +1,40 @@
-let main= document.getElementById('container');
-
+let main = document.getElementById('container');
 let url = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries`
-async function getData(){
 
-    let res = await fetch(url)
-    let data= await res.json();
-    data = data.data
-    console.log(data)
-    displayData(data)
+async function getData(url) {
+    let res = await fetch(url);
+    let data = await res.json();
+    data = data.data;
+    console.log(data);
+    displayData(data);
 }
-getData();
 
-// {country: 'France', id: 1, Rank: 22, population: 64938716}
-
-function displayData(arr){
-
-    arr.forEach((ele)=>{
-
+function displayData(arr) {
+    main.innerHTML = "";
+    arr.forEach((ele) => {
         let card = document.createElement('div');
+        let country = document.createElement('h1');
+        country.innerText = `Country: ${ele.country}`;
+        let id = document.createElement('h2');
+        id.innerText = `ID: ${ele.id}`;
+        let rank = document.createElement('h3');
+        rank.innerText = `Rank: ${ele.Rank}`;
+        let population = document.createElement('h2');
+        population.innerText = `Population: ${ele.population}`;
 
-        let country = document.createElement('h1')
-        country.innerText= `Country: ${ele.country}`;
-        let id = document.createElement('h2')
-        id.innerText= `ID : ${ele.id}`;
-        let Rank = document.createElement('h3')
-        Rank.innerText= `Rank : ${ele.Rank}`;
-        let population = document.createElement('h2')
-        population.innerText= `Population : ${ele.population}`;
-
-        card.append(country,id,Rank,population);
+        card.append(country, id, rank, population);
         main.append(card);
-    })
+    });
+}
+
+getData(url);
+
+let sort = document.querySelector('#sort');
+sort.addEventListener('input',function(){
+    sortData();
+})
+
+function sortData(){
+    let sortVal=sort.value;
+    getData(`${url}?sort=population&order=${sortVal}`);
 }
